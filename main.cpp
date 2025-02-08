0f1c49084a6cf5f5e5c36f2bdf185dbcdb27c5bb
Felipe Lima <lipezera1836@gmail.com>
2025-02-08 16:09:06 -0300

    feat: ler documento e carregar memória

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <bitset>
+
+#define SIZE_MEMORY 0xFFFF
+uint8_t memory[SIZE_MEMORY];
+
+void carregarMemoria(char *file_name)
+{
+    std::ifstream file(file_name);
+
+    if (!file)
+    {
+        std::cout << "Erro ao abir arquivo\n";
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        uint16_t address, data;
+        std::stringstream ss(line);
+        char separador;
+        if (ss >> std::hex >> address >> separador >> std::hex >> data && separador == ':')
+        {
+            memory[address] = data;
+            memory[++address] = (data >> 8);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "Argumentos inválidos\n";
+        return 1;
+    }
+
+    carregarMemoria(argv[1]);
+
+    for (int i = 0; i <= SIZE_MEMORY; i++)
+    {
+        if (memory[i])
+            printf("0x%04X: 0x%02X\n", i, memory[i]);
+    }
+}
